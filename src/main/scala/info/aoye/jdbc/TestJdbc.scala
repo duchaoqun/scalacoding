e50f07bc034662cb4d29f9614b5cce574d2bf4c3
duchaoqun <duchaoqun@rexen.com.cn>
2018-05-08 21:02:12 +0800

    20180508

--- src/main/scala/info/aoye/jdbc/TestJdbc.scala
+++ src/main/scala/info/aoye/jdbc/TestJdbc.scala
@@ -3,33 +3,49 @@ package info.aoye.jdbc
 import java.sql.{Connection, DriverManager, ResultSet}
 import java.util.Properties
 
+/**
+  * scala使用JDBC操作数据库
+  */
 object TestJdbc extends scala.App {
   //获取项目内配置文件
   val config = new Properties()
   //TODO:这个内容需要学习一下
-  //注意：后面的配置上有一个斜杠
-  val config_file = this.getClass.getResourceAsStream("/jdbc.properties")
-
+  val config_file = this.getClass.getResourceAsStream("/jdbc.properties")  //注意：后面的配置上有一个斜杠
   config.load(config_file)
-
   val str_jdbc = config.getProperty("JDBC")
   val str_username = config.getProperty("USERNAME")
   val str_password = config.getProperty("PASSWORD")
   val str_database = config.getProperty("DATABASE")
-
-
   val str_conn = str_jdbc + "/" + str_database + "?user=" + str_username + "&password=" + str_password
 
-  println(str_conn)
+  //获取项目内配置文件
+  def getConfig:String = {
+    val config:Properties = new Properties()
+    val configFile = this.getClass.getResourceAsStream("jdbc.properties")
+    config.load(configFile)
+    val str_jdbc = config.getProperty("JDBC")
+    val str_username = config.getProperty("USERNAME")
+    val str_password = config.getProperty("PASSWORD")
+    val str_database = config.getProperty("DATABASE")
+    val charsetEncoding = config.getProperty("characterEncoding")
+    val str_conn = str_jdbc + "/" + str_database + "?user=" + str_username + "&password=" + str_password +"&characterEncoding=" + charsetEncoding
 
+    str_conn
+  }
 
   // 加载驱动，在当前类中加载驱动类，貌似是非必须的
   //classOf[com.mysql.jdbc.Driver]
 
   // 创建链接
   val conn = DriverManager.getConnection(str_conn)
+  def getConn(jdbcStr:String):Connection={
+    DriverManager.getConnection(jdbcStr)
+  }
+  def closeConn(conn:Connection):Unit={
+    conn.close
+  }
 
-
+  //执行查询语句
   try {
     // 准备查询语句
     val statment = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
@@ -55,18 +71,26 @@ object TestJdbc extends scala.App {
 
 
   //执行插入语句
-  //TODO：需要实际搞一搞
-  //    try {
-  //      val prep = JDBC.getConn.prepareStatement("INSERT INTO quotes (quote, author) VALUES (?, ?) ")
-  //      prep.setString(1, "Nothing great was ever achieved without enthusiasm.")
-  //      prep.setString(2, "Ralph Waldo Emerson")
-  //      prep.executeUpdate
-  //    } catch{
-  //      case e:Exception =>e.printStackTrace
-  //    }
-  //    finally {
-  //      JDBC.closeConn
-  //    }
+/*  def insertObj(obj:GaoDeWeatherInfo,conn:Connection) = {
+    try{
+      val prep = conn.prepareStatement("insert into gaode_weatherinfo(province,city,adcode,weather,temperature,winddirection,windpower,humidity,reporttime) values(?,?,?,?,?,?,?,?,?)")
+      prep.setString(1,obj.province)
+      prep.setString(2,obj.city)
+      prep.setString(3,obj.adcode)
+      prep.setString(4,obj.weather)
+      prep.setString(5,obj.temperature)
+      prep.setString(6,obj.winddirection)
+      prep.setString(7,obj.windpower)
+      prep.setString(8,obj.humidity)
+      prep.setString(9,obj.reporttime)
+
+      prep.executeUpdate()
+    }catch{
+      case e:Exception => e.printStackTrace()
+    } finally {
+      println("OK")
+    }
+  }*/
 
   //todo: 如果执行类是一个伴生对象,那么这个连接会持续到什么时候? 程序退出在释放吗?
   conn.close()
